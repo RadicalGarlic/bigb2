e2c9beca6b15edfabd6da5e552e42d375172f307
RadicalGarlic <nobody@nowhere>
2022-03-24 17:24:23 -0700

    Added unit test stubs

---
+++ src/test/java/bbb2/BackblazeB2ApiProxyTest.java
@@ -0,0 +1,12 @@
+package bbb2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BackblazeB2ApiProxyTest
+{
+    @Test
+    public void test()
+    {
+    }
+}
