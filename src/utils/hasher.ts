4ec5a87b49d764697fb313ce4d3fa27bbeedd516
lmao <rotatewild@gmail.com>
2025-12-21 17:54:47 -0800

    Tighten inputs/outputs on Hasher

--- src/utils/hasher.ts
+++ src/utils/hasher.ts
@@ -1,7 +1,32 @@
+import { Bigb2Error } from 'bigb2-error';
 import * as crypto from 'node:crypto';
 
-export function sha1Hex(buf: Buffer): string {
-  const hash: crypto.Hash = crypto.createHash('sha1');
+export type HashAlgorithm = 'sha1';
+
+export type HashOutputEncoding = 'hex';
+
+export class HashOutput {
+  constructor(public buf: Buffer, public alg: HashAlgorithm) { }
+  public toString(encoding: HashOutputEncoding): string {
+    let encodingConstant: BufferEncoding | null = null;
+    if (encoding === 'hex') {
+      encodingConstant = 'hex';
+    } else {
+      throw new Bigb2Error(`Unexpected hash output format "${encoding}"`);
+    }
+    return this.buf.toString(encodingConstant);
+  }
+}
+
+export function hash(buf: Buffer, alg: HashAlgorithm): HashOutput {
+  let algString = '';
+  if (alg === 'sha1') {
+    algString = 'sha1';
+  } else {
+    throw new Bigb2Error(`Unexpected hash algorithm "${alg}"`);
+  }
+
+  const hash: crypto.Hash = crypto.createHash(algString);
   hash.update(buf);
-  return hash.digest('hex');
+  return new HashOutput(hash.digest(), alg);
 }
