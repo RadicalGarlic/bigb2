95dff88fd230ad567279f0c814073075b513e3b8
lmao <rotatewild@gmail.com>
2025-12-22 18:00:28 -0800

    Clean up DownloadProgress

--- src/operations/download-operation/download-progress.ts
+++ src/operations/download-operation/download-progress.ts
@@ -4,6 +4,7 @@ import * as path from 'node:path';
 import { fullRead } from 'utils/file-full-read';
 import { hash } from 'utils/hasher';
 import { filePathExists } from '../../utils/file-path-exists';
+import { Bigb2Error } from 'bigb2-error';
 
 export interface DownloadProgressChunk {
   startByte: number;
@@ -11,44 +12,54 @@ export interface DownloadProgressChunk {
   hash: string;
 }
 
-export interface DownloadProgressChunks {
-  absoluteFilePath: string,
-  chunks: DownloadProgressChunk[],
+interface DownloadProgressFile {
+  downloadFilePath: string;
+  chunks: DownloadProgressChunk[];
 }
 
 export class DownloadProgress {
-  constructor(public chunks: DownloadProgressChunks, public progressFilePath: string) { }
+  constructor(
+    public downloadFilePath: string,
+    public progressFilePath: string,
+    public chunks: DownloadProgressChunk[],
+  ) { }
 
   public static async initFromFiles(
     downloadFilePath: string,
     downloadProgressFilePath?: string
   ): Promise<DownloadProgress> {
-    downloadProgressFilePath = downloadProgressFilePath ?? `.${downloadProgressFilePath}.download-progress.json`;
-    if (!downloadProgressFilePath || !filePathExists(downloadProgressFilePath)) {
-      return new DownloadProgress(
-        {
-          absoluteFilePath: path.resolve(downloadFilePath),
-          chunks: []
-        },
-        downloadProgressFilePath,
-      );
+    const downloadAbsFilePath = path.resolve(downloadFilePath)
+    const downloadProgressAbsFilePath = path.resolve(downloadProgressFilePath ?? `.${downloadProgressFilePath}.download-progress.json`);
+    if (!filePathExists(downloadProgressAbsFilePath)) {
+      return new DownloadProgress(downloadAbsFilePath, downloadProgressAbsFilePath, []);
     }
-    const downloadProgressChunksJson: string = await fsPromises.readFile(
-      downloadProgressFilePath,
+
+    const downloadProgressFileJson: string = await fsPromises.readFile(
+      downloadProgressAbsFilePath,
       { encoding: 'utf-8' }
     );
-    return new DownloadProgress(JSON.parse(downloadProgressChunksJson), downloadProgressFilePath);
+    const downloadProgressFileObj: DownloadProgressFile = JSON.parse(downloadProgressFileJson);
+    if (downloadAbsFilePath !== downloadProgressFileObj.downloadFilePath) {
+      throw new Bigb2Error(
+        `Mismatch between given downloadFilePath "${downloadAbsFilePath}" and progress file (${downloadProgressAbsFilePath}) downloadFilePath "${downloadProgressFileObj.downloadFilePath}"`
+      );
+    }
+    return new DownloadProgress(downloadAbsFilePath, downloadProgressAbsFilePath, downloadProgressFileObj.chunks);
   }
 
   public async writeToFile(filePath?: string) {
+    const payload: DownloadProgressFile = {
+      downloadFilePath: this.downloadFilePath,
+      chunks: this.chunks,
+    }
     await fsPromises.writeFile(
       filePath ?? this.progressFilePath,
-      JSON.stringify(this.chunks)
+      JSON.stringify(payload),
     );
   }
 
   public recordChunk(startByte: number, chunk: Buffer) {
-    this.chunks.chunks.push({
+    this.chunks.push({
       startByte,
       length: chunk.length,
       hash: hash(chunk, 'sha1').toString('hex'),
@@ -56,47 +67,47 @@ export class DownloadProgress {
   }
 
   public async syncPartiallyDownloadedFile(): Promise<number> {
-    if (this.chunks.chunks.length <= 0) {
+    if (this.chunks.length <= 0) {
       return 0;
     }
     let curVerified = 0;
-    const partialDownloadFile: fsPromises.FileHandle = await fsPromises.open(
-      this.chunks.absoluteFilePath,
+    const partiallyDownloadedFile: fsPromises.FileHandle = await fsPromises.open(
+      this.downloadFilePath,
       'r'
     );
     try {
-      const partialDownloadFileLen = (await partialDownloadFile.stat()).size;
-      for (let chunkIdx = 0; chunkIdx < this.chunks.chunks.length; chunkIdx++) {
-        const curChunk: DownloadProgressChunk = this.chunks.chunks[chunkIdx];
+      const partiallyDownloadedFileLen = (await partiallyDownloadedFile.stat()).size;
+      for (let chunkIdx = 0; chunkIdx < this.chunks.length; chunkIdx++) {
+        const curChunk: DownloadProgressChunk = this.chunks[chunkIdx];
         if (
-          ((curChunk.startByte + curChunk.length) > partialDownloadFileLen)
+          ((curChunk.startByte + curChunk.length) > partiallyDownloadedFileLen)
           || (curChunk.startByte != curVerified)
         ) {
-          this.chunks.chunks = this.chunks.chunks.slice(0, chunkIdx);
-          await partialDownloadFile.truncate(curVerified);
+          this.chunks = this.chunks.slice(0, chunkIdx);
+          await partiallyDownloadedFile.truncate(curVerified);
           return curVerified;
         }
         const buf: Buffer = await fullRead(
-          partialDownloadFile,
+          partiallyDownloadedFile,
           curChunk.startByte,
           curChunk.length
         );
         const testHash: string = hash(buf, 'sha1').toString('hex');
         if (testHash !== curChunk.hash) {
-          this.chunks.chunks = this.chunks.chunks.slice(0, chunkIdx);
-          await partialDownloadFile.truncate(curVerified);
+          this.chunks = this.chunks.slice(0, chunkIdx);
+          await partiallyDownloadedFile.truncate(curVerified);
           return curVerified;
         }
         curVerified += curChunk.length;
       }
 
-      if (curVerified !== partialDownloadFileLen) {
-        await partialDownloadFile.truncate(curVerified);
+      if (curVerified !== partiallyDownloadedFileLen) {
+        await partiallyDownloadedFile.truncate(curVerified);
       }
 
       return curVerified;
     } finally {
-      await partialDownloadFile.close();
+      await partiallyDownloadedFile.close();
     }
   }
 }
