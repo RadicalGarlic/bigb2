27e4937e802deab92b65ee402efcbbad20571a07
lmao <rotatewild@gmail.com>
2025-12-27 11:57:41 -0800

    UploadFile added

--- src/b2-api/calls/upload-file.ts
+++ src/b2-api/calls/upload-file.ts
@@ -1,56 +1,76 @@
-// import fetch, { Response } from 'node-fetch';
-// import * as crypto from 'crypto';
+import * as https from 'node:https';
+import * as http from 'node:http';
 
-// import { AUTHORIZATION } from 'src/constants';
+import { B2ApiError } from 'b2-api/b2-api-error';
+import { assertPrimitiveField } from 'utils/assert-primitive-field';
 
-// export class UploadFileRequest {
-//   constructor(
-//     private uploadUrl: URL,
-//     private uploadAuthToken: string,
-//     private dstFilePath: string,
-//     private srcData: Buffer
-//   ) { }
+export interface UploadFileResponse {
+  fileId: string;
+  bucketId: string;
+  contentLength: number;
+  contentSha1: string;
+  fileName: string;
+}
 
-//   async send(): Promise<UploadFileResponse> {
-//     const sha1 = crypto.createHash('sha1');
-//     sha1.update(new DataView(this.srcData.buffer));
-//     const hash = sha1.digest('hex');
+export class UploadFileRequest {
+  constructor(private args: {
+    apiUrl: URL,
+    authToken: string,
+    fileName: string,
+    contentType: string,
+    contentLength: number,
+    contentSha1: string,
+  }) { }
 
-//     const res: Response = await fetch(
-//       this.uploadUrl.toString(),
-//       {
-//         method: 'POST',
-//         headers: {
-//           AUTHORIZATION: this.uploadAuthToken,
-//           'X-Bz-File-Name': encodeURI(this.dstFilePath),
-//           'Content-Type': 'application/octet-stream',
-//           'Content-Length': this.srcData.byteLength.toString(),
-//           'X-Bz-Content-Sha1': hash
-//         },
-//         body: this.srcData.buffer
-//       }
-//     );
-
-//     return Promise.resolve(UploadFileResponse.fromJson(await res.text()));
-//   }
-// }
-
-// export class UploadFileResponse {
-//   constructor(
-//     public readonly fileId: string,
-//     public readonly fileName: string,
-//     public readonly sha1Hex: string
-//   ) { }
-
-//   static fromJson(json: string): UploadFileResponse {
-//     const obj = JSON.parse(json);
-//     if (
-//       (typeof(obj.fileId) !== 'string')
-//       || (typeof(obj.fileName) !== 'string')
-//       || (typeof(obj.contentSha1) !== 'string')
-//     ) {
-//       throw new Error(`UploadFileResponse bad field(s), ${json}`);
-//     }
-//     return new UploadFileResponse(obj.fileId, obj.fileName, obj.contentSha1);
-//   }
-// }
+  async send(): Promise<UploadFileResponse> {
+    return new Promise<UploadFileResponse>(
+      (resolve, reject) => {
+        const req: http.ClientRequest = https.request(
+          this.args.apiUrl,
+          {
+            headers: { Authorization: this.args.authToken },
+            method: 'POST'
+          }
+        );
+        req.on('error', (err: Error) => {
+          return reject(new B2ApiError('UploadFile failed', { cause: err }));
+        });
+        req.on('response', (res: http.IncomingMessage) => {
+          const resChunks: Buffer[] = [];
+          res.on('error', (err: Error) => {
+            return reject(new B2ApiError('UploadFile error', { cause: err }));
+          });
+          res.on('data', (chunk: Buffer) => { resChunks.push(chunk); });
+          res.on('end', () => {
+            if (!res.complete) {
+              return reject(new B2ApiError('UploadFile interrupted'));
+            }
+            const resBodyJson: string = Buffer.concat(resChunks).toString('utf-8');
+            const resBodyObj = JSON.parse(resBodyJson);
+            if (B2ApiError.isB2ApiError(resBodyObj) || res.statusCode !== 200) {
+              return reject(B2ApiError.fromObj(resBodyObj, 'UploadFile failed'));
+            }
+            try {
+              return resolve({
+                fileId: String(assertPrimitiveField(resBodyObj, 'fileId', 'string')),
+                fileName: String(assertPrimitiveField(resBodyObj, 'fileName', 'string')),
+                bucketId: String(assertPrimitiveField(resBodyObj, 'bucketId', 'string')),
+                contentLength: Number(assertPrimitiveField(resBodyObj, 'contentLength', 'number')),
+                contentSha1: String(assertPrimitiveField(resBodyObj, 'contentSha1', 'string')),
+              });
+            } catch (err: unknown) {
+              if (err instanceof Error) {
+                return reject(new B2ApiError(
+                  `Failed to parse UploadFile response. JSON=${resBodyJson}`,
+                  { cause: err }
+                ));
+              }
+              return reject(err);
+            }
+          });
+        });
+        req.end();
+      }
+    )
+  }
+}
