ce29ec8a04bcc9f02ad6b54f8c26ddca5e33fd27
lmao <rotatewild@gmail.com>
2025-12-27 13:04:55 -0800

    Fix parsing on ListUnfinishedLargeFiles

--- src/b2-api/calls/list-unfinished-large-files.ts
+++ src/b2-api/calls/list-unfinished-large-files.ts
@@ -2,8 +2,9 @@ import * as https from 'node:https';
 import * as http from 'node:http';
 
 import { UrlProvider } from 'b2-iface/url-provider';
-import { Range } from 'b2-iface/range';
 import { B2ApiError } from 'b2-api/b2-api-error';
+import { B2Api } from 'b2-api/b2-api';
+import { assertPrimitiveField } from 'utils/assert-primitive-field';
 
 export interface UnfinishedLargeFile {
   accountId: string;
@@ -16,7 +17,7 @@ export interface UnfinishedLargeFile {
 
 export interface ListUnfinishedLargeFilesResponse {
   files: UnfinishedLargeFile[];
-  nextFileId: string;
+  nextFileId: string | null;
 }
 
 export class ListUnfinishedLargeFilesRequest {
@@ -33,13 +34,26 @@ export class ListUnfinishedLargeFilesRequest {
     return new Promise<ListUnfinishedLargeFilesResponse>(
       (resolve, reject) => {
         const url: URL = UrlProvider.listUnfinishedLargeFilesUrl(this.args.apiUrl);
+        url.searchParams.append('bucketId', this.args.bucketId);
+        if (this.args.namePrefix) {
+          url.searchParams.append('namePrefix', this.args.namePrefix);
+        }
+        if (this.args.startFileId) {
+          url.searchParams.append('startFileId', this.args.startFileId);
+        }
+        if (this.args.maxFileCount) {
+          url.searchParams.append('maxFileCount', String(this.args.maxFileCount));
+        }
         const req: http.ClientRequest = https.request(
           url,
           {
             headers: { Authorization: this.args.authToken },
-            method: 'GET'
+            method: 'GET',
           }
         );
+        req.on('error', (err: Error) => {
+          return reject(new B2ApiError('ListUnfinishedLargeFiles error', { cause: err }));
+        })
         req.on('response', (res: http.IncomingMessage) => {
           const resChunks: Buffer[] = [];
           res.on('error', (err: Error) => {
@@ -53,21 +67,20 @@ export class ListUnfinishedLargeFilesRequest {
             const resBodyJson: string = Buffer.concat(resChunks).toString('utf-8');
             const resBodyObj = JSON.parse(resBodyJson);
             if (B2ApiError.isB2ApiError(resBodyJson) || res.statusCode !== 200) {
-              return reject(B2ApiError.fromJson(resBodyJson));
+              return reject(B2ApiError.fromObj(resBodyObj, 'ListUnfinishedLargeFiles failed'));
             }
             try {
               return resolve({
+                nextFileId: resBodyObj.nextFileId ?? null,
                 files: resBodyObj!.files!.map((value: any) => {
                   return {
-                    accountId: value!.accountId,
-                    action: value!.action,
-                    bucketId: value!.bucketId,
-                    fileId: value!.fileId,
-                    fileName: value!.fileName,
-                    contentLength: value!.contentLength,
-                  }
+                    accountId: String(assertPrimitiveField(value, 'accountId', 'string')),
+                    action: String(assertPrimitiveField(value, 'action', 'string')),
+                    fileId: String(assertPrimitiveField(value, 'fileId', 'string')),
+                    fileName: String(assertPrimitiveField(value, 'fileName', 'string')),
+                    contentLength: Number(assertPrimitiveField(value, 'contentLength', 'number')),
+                  };
                 }),
-                nextFileId: resBodyObj!.nextFileId,
               });
             } catch (err: unknown) {
               if (err instanceof Error) {
