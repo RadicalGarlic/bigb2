c15f6954321b55452816db5fe2afd57ec74f7ada
lmao <rotatewild@gmail.com>
2026-01-13 21:17:14 -0800

    CopyPart ready for testing

--- src/b2-api/calls/copy-part.ts
+++ src/b2-api/calls/copy-part.ts
@@ -1,12 +1,17 @@
-import * as https from 'node:https';
 import * as http from 'node:http';
-
-import * as t from 'io-ts';
-import { PathReporter } from 'io-ts/PathReporter';
-import { isLeft } from 'fp-ts/lib/Either';
+import * as https from 'node:https';
 
 import { UrlProvider } from 'b2-iface/url-provider';
 import { ByteRange } from 'b2-api/utils/byte-range';
+import { B2ApiError } from 'b2-api/b2-api-error';
+import { B2Api } from 'b2-api/b2-api';
+import { peckPrimitiveField } from 'utils/peck';
+
+export interface CopyPartResponse {
+  fileId: string,
+  partNumber: number,
+  contentLength: number,
+}
 
 export class CopyPartRequest {
   constructor(private args: {
@@ -18,8 +23,8 @@ export class CopyPartRequest {
     partNumber: number,
   }) { }
 
-  async send(): Promise<CopyPartResponseType> {
-    return new Promise<CopyPartResponseType>(
+  async send(): Promise<CopyPartResponse> {
+    return new Promise<CopyPartResponse>(
       (resolve, reject) => {
         const url: URL = UrlProvider.startLargeFileUrl(this.args.apiUrl);
         const req: http.ClientRequest = https.request(
@@ -29,38 +34,47 @@ export class CopyPartRequest {
             method: 'POST'
           }
         );
+        req.on('error', (err: Error) => {
+          return reject(new B2ApiError('CopyPart call failed', { cause: err }));
+        });
         req.on('response', (res: http.IncomingMessage) => {
-          const resChunks: string[] = [];
-          res.on('error', (err: Error) => { reject(err); });
-          res.on('data', (chunk: string) => { resChunks.push(chunk); });
+          const resChunks: Buffer[] = [];
+          res.on('error', (err: Error) => {
+            return reject(new B2ApiError('CopyPart call failed', { cause: err }));
+          });
+          res.on('data', (chunk: Buffer) => { resChunks.push(chunk); });
           res.on('end', () => {
-            const resBody: string = resChunks.join('');
-            const decoded = CopyPartResponse.decode(JSON.parse(resBody));
-            if (isLeft(decoded)) {
-              reject(new Error(`Could not validate data: ${PathReporter.report(decoded).join('\n')}`));
-            } else {
-              resolve(decoded.right);
+            if (!res.complete) {
+              return reject(new B2ApiError('CopyPart interrupted'));
             }
+            const resBodyString: string = Buffer.concat(resChunks).toString('utf-8');
+            const resBodyObj = JSON.parse(resBodyString);
+            if (B2ApiError.isB2ApiError(resBodyString)) {
+              return reject(B2ApiError.fromJson(resBodyString, 'CopyPart error'));
+            }
+            return resolve({
+              fileId: String(peckPrimitiveField(resBodyObj, 'fileId', 'string')),
+              partNumber: Number(peckPrimitiveField(resBodyObj, 'partNumber', 'number')),
+              contentLength: Number(peckPrimitiveField(resBodyObj, 'contentLength', 'number')),
+            });
           });
         });
-        req.on('error', (err: Error) => { reject(err); });
-        req.write(JSON.stringify({
-          sourceFileId: this.args.srcFileId,
-          largeFileId: this.args.dstLargeFileId,
-          partNumber: this.args.partNumber,
-          range: this.args.range ? this.args.range.toString() : undefined
-        }));
+        req.write(
+          JSON.stringify({
+            sourceFileId: this.args.srcFileId,
+            largeFileId: this.args.dstLargeFileId,
+            partNumber: this.args.partNumber,
+            range: this.args.range ? this.args.range.toString() : undefined
+          }),
+          (err: Error | null | undefined) => {
+            if (err instanceof Error) {
+              return reject(new B2ApiError('CopyPart error', { cause: err }));
+            }
+            // success!
+          }
+        );
         req.end();
       }
     )
   }
 }
-
-const CopyPartResponse = t.type({
-  fileId: t.string,
-  partNumber: t.Int,
-  contentLength: t.Int,
-});
-export type CopyPartResponseType = t.TypeOf<
-  typeof CopyPartResponse
->;
