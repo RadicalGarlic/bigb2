4c5b6a6c70f7212471e9d9bb0dcbda5f717fc2a7
lmao <rotatewild@gmail.com>
2026-01-13 22:23:13 -0800

    Removed TODO for assert check naming
    Peck it is!

--- src/main.ts
+++ src/main.ts
@@ -50,4 +50,3 @@ main()
 // TODO: https://zod.dev/
 // TODO: Less reliance on "!", or at least ensure it throws good exception. Non-null version of classes?
 // TODO: content-type string constants
-// TODO: Better name for your assert checks. Maybe not if we're going to zod.
