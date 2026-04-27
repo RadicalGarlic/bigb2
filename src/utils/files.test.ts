de1403075692373e5325563d49f96493150f18c2
lmao <rotatewild@gmail.com>
2026-04-26 18:09:31 -0700

    Set up filesystem tests

---
+++ src/utils/files.test.ts
@@ -0,0 +1,23 @@
+import { beforeEach, expect, test, vi } from 'vitest'
+vi.mock('node:fs');
+vi.mock('node:fs/promises');
+import { fs, vol } from 'memfs'
+
+import { filePathExists } from './files';
+
+beforeEach(() => {
+  // reset the state of in-memory fs
+  vol.reset()
+});
+
+test('filePathExists does not exist', async () => {
+  const filename = '/not_found';
+  await fs.promises.rm(filename, { force: true });
+  expect(await filePathExists(filename)).toBe(false);
+});
+
+test('filePathExists does not exist', async () => {
+  const filename = '/file';
+  await fs.promises.writeFile(filename, '');
+  expect(await filePathExists(filename)).toBe(true);
+});
